9d90a4f7ca34624ae77972945fa381b54d92eaa8
ogungbile <oluwatobi.ogungbile@yahoo.com>
2022-06-08 20:06:38 -0700

    function to print alphabet lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * printing lowercase of the alphabet
+ *
+ */
+void print_alphabet(void)
+{
+	int b;
+	for (b = 'a' ; b <= 'z' ; b++ )
+{
+	_putchar(b);
+}
+	_putchar ('\n');
+}
