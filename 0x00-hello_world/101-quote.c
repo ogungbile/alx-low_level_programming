ae14488f2789966e64dc8846dd7f66a754fe7a6a
ogungbile <oluwatobi.ogungbile@yahoo.com>
2022-09-08 13:29:12 -0700

    Fix 2 for  101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,17 +2,13 @@
 #include <unistd.h>
 
 /**
+ *  main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- * 
+ *  Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
