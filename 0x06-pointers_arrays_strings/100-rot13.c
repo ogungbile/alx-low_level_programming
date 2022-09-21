1679cf0433c56af76fb0006259f3457f88cbeb54
ogungbile <oluwatobi.ogungbile@yahoo.com>
2022-09-21 10:24:19 -0700

    Fix 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,9 +8,9 @@
 char *rot13(char *s)
 {
 	int i, j;
-
 	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
 	for (i = 0; *(s + i); i++)
 	{
 		for (j = 0; j < 52; j++)
