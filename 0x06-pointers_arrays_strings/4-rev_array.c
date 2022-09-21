0a4f40c55edf741b4ab05b71c82d75450a907d71
ogungbile <52929020+ogungbile@users.noreply.github.com>
2022-09-21 18:52:03 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,21 +1,22 @@
 #include "main.h"
-/**
- * reverse_array -> reversing array
- * @a: array to be reversed
- * @n: number of elements of the array
- */
-void  reverse_array(int *a, int n)
-{
-	int *p, i, aux, k;
 
-	p = a;
-	for (i = 0; i < n; i++)
-		p++;
-	for (k = 0; k <= n / 2; k++)
+/**
+ * reverse_array - check the code for Holberton School students.
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	n--;
+
+	for (i = 0; i < n; i++, n--)
 	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
 	}
 }
