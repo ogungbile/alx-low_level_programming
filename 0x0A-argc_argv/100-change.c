e55313202536c2dfd049a1ff01ca90ed5c8f1fca
ogungbile <52929020+ogungbile@users.noreply.github.com>
2022-10-05 00:58:40 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdbool.h>
+
+
 /**
  * coinConverter - Helper function that does all the mathematics
  * @i: Passed in variable from main for calculations
- *
  * Return: The number of coins needed minimum for the passed in variable
  */
+
 int coinConverter(int i)
 {
 	int count = 0;
@@ -16,6 +18,7 @@ int coinConverter(int i)
 	{
 		if (i % 10 == 9 || i % 10 == 7)
 			i -= 2;
+
 		else if (i % 25 == 0)
 			i -= 25;
 		else if (i % 10 == 0)
@@ -31,33 +34,40 @@ int coinConverter(int i)
 		}
 		else
 			i -= 1;
+
 		count++;
 	}
+
 	return (count);
 }
+
 /**
  * main - Takes in exactly one argument for minimum coin count
  * @argc: Number of command line arguments
  * @argv: Array name
- *
  * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
  */
+
 int main(int argc, char *argv[])
 {
 	int i, coin;
 
 	coin = 0;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
 	i = atoi(argv[1]);
+
 	if (i < 0)
 		printf("0\n");
 	else
 	{
 		coin = coinConverter(i);
+
 		printf("%d\n", coin);
 	}
 	return (0);
