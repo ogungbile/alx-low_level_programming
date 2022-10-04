c77e107f52f5ce8a33f2d10ff130ab3e82a1bbfb
ogungbile <oluwatobi.ogungbile@yahoo.com>
2022-10-04 16:43:59 -0700

    Create static library

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat -> function to merge some characters
+ * @dest: first argument
+ * @src: second argument
+ * @n: third argument
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
+
