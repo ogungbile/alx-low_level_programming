8a8288983abed150099271e8194bb8af371db821
ogungbile <52929020+ogungbile@users.noreply.github.com>
2022-10-26 03:19:36 +0100

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * find_listint_loop - finds the loop contained
  *                     in a linked list
@@ -6,6 +7,7 @@
  * Return: pointer the node where the loop starts,
  *         or NULL - if there is no loop
  */
+
 listint_t *find_listint_loop(listint_t *head)
 {
 listint_t *node1, *node2;
