86ee0e17864a62d598ca10ee89bd53459594c3c7
ogungbile <52929020+ogungbile@users.noreply.github.com>
2023-01-11 20:16:01 +0100

    Update 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+
 /**
  * hash_djb2 - Hash function implementing the djb2 algorithm.
  * @str: The string to hash.
@@ -9,10 +10,10 @@ unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
-	
+
 	hash = 5381;
 	while ((c = *str++))
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	
+
 	return (hash);
 }
