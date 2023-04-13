b087cf811e8236dcccf3b6e49cf23a68e3d627c8
ogungbile <oluwatobi.ogungbile@yahoo.com>
2023-04-13 06:06:18 -0700

    Fix the prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,11 +1,10 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-#include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
+ * struct listint_s - singly linked list
  *
  * @n: Integer
  * @index: Index of the node in the list
@@ -15,9 +14,9 @@
  */
 typedef struct listint_s
 {
-		int n;
-		size_t index;
-		struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
 /**
@@ -32,12 +31,13 @@ typedef struct listint_s
  */
 typedef struct skiplist_s
 {
-				int n;
-				size_t index;
-				struct skiplist_s *next;
-				struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
+/* prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
@@ -48,4 +48,4 @@ listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
 
-#endif /* SEARCH_ALGO_H */
+#endif /* Search algos */
